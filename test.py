f0ba838fed6e3726d66d948e33ed0013f546097d
Kaapstadt <hiltonhugo@gmail.com>
2021-05-15 11:37:21 +0200

    Changed test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ cursor = connection.cursor()
 create_table = 'CREATE TABLE users(id int, username text, password text)'
 cursor.execute(create_table)
 
-user = (1,'brian','password')
+user = (1,'brianhugo','password')
 insert_query = 'INSERT INTO users VALUES(?, ?, ?)'
 cursor.execute(insert_query,user)
 
